b74871b86f2871c542bdd8ceb71a14e15410ff63
Deveshhanwat <deveshhanwat19@gmail.com>
2026-04-26 11:49:00 +0530

    Gameautomation.py

---
+++ Gameautomation.py
@@ -0,0 +1,52 @@
+from itertools import product
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+# Setting up Chrome options
+options = Options()
+options.add_experimental_option("detach", True)
+
+# Initializing the Chrome driver with options
+driver = webdriver.Chrome(options=options)
+driver.maximize_window()
+
+# Opening Flipkart
+driver.get("https://orteil.dashnet.org/cookieclicker/")
+
+cookie_id = "bigCookie"
+cookies_id = "cookies"
+product_price_prefix = "productPrice"
+product_prefix = "product"
+
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'English')]")))
+
+language = driver.find_element(By.XPATH, "//*[contains(text(), 'English')]")
+language.click()
+
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.ID, cookie_id)))
+
+cookie = driver.find_element(By.ID, cookie_id)
+
+while True:
+    cookie.click()
+    cookies_count = driver.find_element(By.ID, cookies_id).text.split(" ")[0]
+    cookies_count = int(cookies_count.replace(",", ""))
+
+    for i in range(4):
+        product_price = driver.find_element(By.ID, product_price_prefix + str(i)).text.replace(",", "")
+
+        if not product_price.isdigit():
+            continue
+
+        product_price = int(product_price)
+
+        if cookies_count >= product_price:
+            product = driver.find_element(By.ID, product_prefix + str(i))
+            product.click()
+            break
